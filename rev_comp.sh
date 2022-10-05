c9d3ce0f0d2e433d4832ef3fd08b1bb10a3fa603
cmm0172 <cmm0172@auburn.edu>
2022-10-05 09:39:48 -0500

    Added sample fasta doc and also changed name of script per instructions. - Chloe

---
+++ rev_comp.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+#initialize variable
+reverse=""
+
+#read in data
+name=`grep ">" $1`
+sequence=`grep -v ">" $1`
+
+echo "Name: $name"
+
+#calculate length of sequence
+len=${sequence}
+
+echo "Length: $len"
+
+#loop through sequence in reverse
+#for (( i=$len; i>=0; i-- ))
+#do
+ #   reverse="$reverse${sequence:$i:1}"
+#done
+#jordan
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+        echo "Error: add one argument to input"
+        else echo "argument is 1"
+        exit
+fi
+
+rev $sequence | tr [aA] 'atcg' 'tagc' $sequence > rc.$1.txt
+
+#echo "$sequence
+#$reverse
+#"
+
+#complement sequence
+#rc=`echo $reverse | tr 'atcg' 'tagc'`
+
+#echo "$rc
+#"
+
+
+#echo $name >$1.rc.txt
+#echo $rc >>$1.rc.txt
+
+
